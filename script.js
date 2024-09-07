e016522caa433491f9c94bc4c6f9f6dcc37f9b83
Mateus Dourado <166462181+MateusD77@users.noreply.github.com>
2024-09-07 18:27:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+    let section = document.getElementById
+    ("resultados-pesquisa");
     console.log(section); // Loga o elemento HTML para fins de depuração
-  
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+  //se o campoPesquisa for uma string sem nada
+  if(!campoPesquisa){
+      section.innerHTML = "<p> Nenhum Jogo encontrado! </p>"
+      return
+  }
+    console.log(campoPesquisa);
     // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-  
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
     // Itera sobre cada dado na lista de jogos
     for (let dado of jogos) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      if(titulo.includes(campoPesquisa) || 
+      descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+console.log(dado.titulo.includes(campoPesquisa))
       // Constrói o HTML para cada resultado, formatando os dados
       resultados += `
         <div class="item-resultado">
@@ -16,8 +36,13 @@ function pesquisar() {
           <a href="${dado.link}" target="_blank">Saiba Mais...</a>
         </div>
       `;
+      }
+      
+    }
+    if (!resultados) {
+      resultados = "<p> Pequisa não encontrada...Digite algo relacionado ao Jogo! </p>"
     }
   
     // Atribui o HTML gerado para a seção de resultados
     section.innerHTML = resultados;
-  }
+  };

67dcacb24bb1589993448e3ab537d998a5c499af
Mateus Dourado <166462181+MateusD77@users.noreply.github.com>
2024-09-07 18:26:36 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,46 +2,56 @@ let jogos= [
     {
         titulo : "Minecraft",
         descricao :"Minecraft é um jogo eletrônico sandbox de mundo aberto que permite aos jogadores explorar, construir e criar em um ambiente virtual composto por blocos. Com uma liberdade quase ilimitada, os jogadores podem construir desde simples abrigos até cidades complexas, utilizando uma variedade de materiais. Além da construção, o jogo oferece a possibilidade de explorar diferentes biomas, minerar recursos, criar ferramentas e lutar contra criaturas hostis. A versatilidade e a criatividade são os pilares de Minecraft, tornando-o um dos jogos mais populares e duradouros de todos os tempos.",
-        link : "https://www.minecraft.net/pt-br"
+        link : "https://www.minecraft.net/pt-br",
+        tags: "Minecraft, jogo, videogame, sandbox, construção, mundo aberto, blocos, sobrevivência, criativo"
     },
     {
         titulo : "Valorant",
         descricao :"Valorant é um jogo de tiro tático em primeira pessoa gratuito, desenvolvido e publicado pela Riot Games. O jogo coloca dois times de cinco jogadores um contra o outro em partidas competitivas, com cada jogador escolhendo um agente único com habilidades especiais para ajudar seu time a vencer. Valorant se destaca por seu gameplay preciso e competitivo, com um sistema de agentes que adiciona profundidade estratégica ao jogo.",
-        link : "https://playvalorant.com/pt-br/"
+        link : "https://playvalorant.com/pt-br/",
+        tags : "Valorant, FPS, tático, competitivo, agente, habilidade, round, mapa, raze, jett, phoenix"
     },
     {
         titulo : "Fortnite Battle Royale",
         descricao :"Fortnite Battle Royale é um jogo de tiro em terceira pessoa gratuito, onde até 100 jogadores são lançados em uma ilha e competem para ser o último sobrevivente. Os jogadores devem explorar a ilha, encontrar armas e itens, construir estruturas para se proteger e eliminar seus oponentes. O jogo é conhecido por suas constantes atualizações, que adicionam novos mapas, armas, modos de jogo e colaborações com outras franquias.",
-        link : "https://www.fortnite.com/?lang=pt-BR"
+        link : "https://www.fortnite.com/?lang=pt-BR",
+        tags : "Fortnite, Battle Royale, shooter, construção, gratuito, temporada, skin, zona, loot, crossplay"
     },
     {
         titulo: "FIFA",
         descricao: "FIFA é uma franquia de jogos eletrônicos de futebol que simula o esporte mais popular do mundo. Com gráficos realistas e jogabilidade dinâmica, os jogadores podem controlar seus times favoritos, participar de campeonatos e criar suas próprias ligas. O FIFA oferece uma experiência autêntica de futebol, com modos de jogo variados e atualizações constantes.",
-        link: "https://www.ea.com/pt-br/games/fifa"
+        link: "https://www.ea.com/pt-br/games/fifa",
+        tags : "FIFA, futebol, esporte, simulador, jogador, time, campeonato, carreira, Ultimate Team, transferências"
     },
     {
         titulo: "Far Cry",
         descricao: "Far Cry é uma série de jogos de tiro em primeira pessoa ambientada em cenários exóticos e perigosos. Os jogadores assumem o papel de protagonistas que precisam sobreviver e enfrentar diversas ameaças, como animais selvagens e grupos inimigos. A franquia Far Cry é conhecida por seus gráficos impressionantes, jogabilidade intensa e história envolvente.",
-        link: "https://www.ubisoft.com/pt-br/"
+        link: "https://www.ubisoft.com/pt-br/",
+        tags : "Far Cry, mundo aberto, shooter, campanha, vilão, selva, armas, exploração, história, mercenário"
     },
     {
         titulo: "Rocket League",
         descricao: "Rocket League é um jogo de esportes veicular que combina elementos de futebol e automobilismo. Os jogadores controlam carros equipados com foguetes e competem em partidas frenéticas para marcar gols. O jogo oferece uma experiência divertida e competitiva, com modos de jogo variados e uma comunidade ativa.",
-        link: "https://www.rocketleague.com/"
+        link: "https://www.rocketleague.com/",
+        tags : "Rocket League, futebol, carro, esporte, online, multiplayer, gol, aréola, boost, derby"
     },
     {
         titulo: "Brawlhalla",
         descricao: "Brawlhalla é um jogo de luta gratuito que apresenta personagens inspirados em diversas culturas e mitologias. Os jogadores podem escolher entre uma variedade de lutadores e participar de combates emocionantes em arenas coloridas. O jogo oferece uma jogabilidade acessível e divertida, com um sistema de combate profundo e estratégico.",
-        link: "https://www.brawlhalla.com/"
+        link: "https://www.brawlhalla.com/",
+        tags : "Brawlhalla, plataforma, luta, gratuito, personagem, skin, arena, combate, online, competitivo"
     },
     {
         titulo: "Call of Duty: Warzone",
         descricao: "Call of Duty: Warzone é um jogo de tiro em primeira pessoa gratuito e battle royale que coloca até 150 jogadores em um mapa gigante para uma batalha até o último sobrevivente. Com uma jogabilidade intensa e um arsenal de armas personalizáveis, Warzone oferece uma experiência emocionante e competitiva.",
-        link: "https://www.callofduty.com/warzone"
+        link: "https://www.callofduty.com/warzone",
+        tags : "Call of Duty, Warzone, Battle Royale, FPS, multiplayer, mapa, armamento, gulag, contrato, squad"
     },
     {
-    titulo: "Guardians of the Galaxy",
-    descricao: "Guardians of the Galaxy é um jogo de ação e aventura que coloca você no papel de Star-Lord, líder dos Guardiões da Galáxia. Explore planetas alienígenas, lute contra inimigos épicos e vivencie uma história emocionante com os personagens mais amados da Marvel.",
-    link: "https://www.xbox.com/pt-BR/games/store/Guardioes-da-Galaxia-da-Marvel/9PGLL77C201J?msockid=154e916f08a96e5a2d9c805d09c26fcc"
-}
+        titulo: "Guardians of the Galaxy",
+        descricao: "Guardians of the Galaxy é um jogo de ação e aventura que coloca você no papel de Star-Lord, líder dos Guardiões da Galáxia. Explore planetas alienígenas, lute contra inimigos épicos e vivencie uma história emocionante com os personagens mais amados da Marvel.",
+        link: "https://www.xbox.com/pt-BR/games/store/Guardioes-da-Galaxia-da-Marvel/9PGLL77C201J?msockid=154e916f08a96e5a2d9c805d09c26fcc",
+        tags : "Guardians of the Galaxy, aventura, ação, Marvel, equipe, espaço, cósmico, história, Star-Lord, Gamora"
+    }
 ];
+
